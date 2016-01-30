1134cfd3dbd202d2678feedc3158e9f0db3f6039
Claudio Freda <claudio.freda93@gmail.com>
2016-01-30 16:56:01 +0100

    aggiunto test (disordinato!) di ragno con braccia fisiche

---
+++ Assets/MousePhysicsTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using Rewired;
+
+public class MousePhysicsTest : MonoBehaviour
+{
+	private TargetJoint2D currentyActiveJoint;
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			var point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			var body = Physics2D.OverlapPointAll(point, SRLayerMask.All).FirstOrDefault(c => c.GetComponent<Rigidbody2D>());
+			if (body != null)
+			{
+				Debug.Log("grabbed body" + body.name);
+                currentyActiveJoint = body.gameObject.AddComponent<TargetJoint2D>();
+				currentyActiveJoint.autoConfigureTarget = false;
+				currentyActiveJoint.anchor = body.transform.InverseTransformPoint(point);
+			}
+			else Debug.Log("didnt grab body");
+		}
+
+		if (currentyActiveJoint != null)
+		{
+			if (Input.GetMouseButton(0))
+			{
+				currentyActiveJoint.target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			}
+			else
+			{
+				Destroy(currentyActiveJoint);
+			}
+		}
+	}
+}

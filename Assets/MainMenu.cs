7b4fbc60ac18188da6be953db598d6c5fe489e3a
Claudio Freda <claudio.freda93@gmail.com>
2016-01-30 20:35:09 +0100

    added main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	void Update()
+	{
+		var system = Rewired.ReInput.players.GetSystemPlayer();
+		if (system.GetButtonDown("Start Game") || (system.GetButton("Left Jump") && system.GetButton("Right Jump")))
+		{
+			SceneManager.LoadScene(SRScenes.SpiderPhotoSession);
+		}
+
+		if (system.GetButtonDown("End Game"))
+		{
+			Application.Quit();
+		}
+	}
+}

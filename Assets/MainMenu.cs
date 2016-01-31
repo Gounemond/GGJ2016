6941cb88827aa72599a03fef420601b34e6bcda2
Claudio Freda <claudio.freda93@gmail.com>
2016-01-31 13:08:30 +0100

    aggiunto audio nel menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,9 +7,11 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
 	public Image fadeImage;
+	private AudioSource source;
 
 	void Start()
 	{
+		source = GetComponent<AudioSource>();
 		fadeImage.DOFade(0, 0.6f);
 	}
 
@@ -19,7 +21,8 @@ public class MainMenu : MonoBehaviour
 		{
 			if (p.GetButtonDown("Start Game") || (p.GetButton("Left Jump") && p.GetButton("Right Jump")))
 			{
-				fadeImage.DOFade(1, 1).OnComplete(() =>
+				source.DOFade(0, 1);
+                fadeImage.DOFade(1, 1).OnComplete(() =>
 				{
 					SceneManager.LoadScene(SRScenes.SpiderPhotoSession);
 				}); 
@@ -27,6 +30,7 @@ public class MainMenu : MonoBehaviour
 
 			if (p.GetButtonDown("End Game"))
 			{
+				source.DOFade(0, 1);
 				fadeImage.DOFade(1, 1).OnComplete(() =>
 				{
 					Application.Quit();
